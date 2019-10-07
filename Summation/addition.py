9511c95cdc1f322d7cff3466bd59bf3b2d580555
BCA-Bizchain-Asset-Tech <55981319+BCA-Bizchain-Asset-Tech@users.noreply.github.com>
2019-10-07 13:22:53 +0530

    Add files via upload
    -Addition between two numbers
    -Numbers can be chosen

---
+++ Summation/addition.py
@@ -0,0 +1,39 @@
+# Source Code
+# This program adds two numbers 
+num1 = 1.5
+num2 = 6.3
+# Add two numbers
+sum = float(num1) + float(num2)
+# Display the sum
+print('The sum of {0} and {1} is {2}'.format(num1, num2, sum))
+# Changing this operator, we can subtract (-), multiply (*), divide (/), floor divide (//) or find the remainder (%) of two numbers.
+# Source Code: Add Two Numbers Provided by The User
+# Store input numbers
+num1 = input('Enter first number: ')
+num2 = input('Enter second number: ')
+# Add two numbers
+sum = float(num1) + float(num2)
+# Display the sum
+print('The sum of {0} and {1} is {2}'.format(num1, num2, sum))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
